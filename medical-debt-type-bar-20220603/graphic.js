4422bf0d3ecca74bc1d2093d78c45297a1d2e36b
Daniel Wood <daniel.j.woo11@gmail.com>
2022-06-03 12:36:51 -0400

    add medical debt bar charts, h3 style to base.less

---
+++ medical-debt-type-bar-20220603/graphic.js
@@ -0,0 +1,61 @@
+var mode = null;
+if (document.querySelector("body").classList.contains("npr")) {
+  mode = "npr";
+}
+if (document.querySelector("body").classList.contains("khn")) {
+  mode = "khn";
+}
+
+var pym = require("./lib/pym");
+
+switch(mode) {
+  case "khn":
+    require("../_base/webfonts_khn.js");
+    break;
+  case "npr":
+    require("../_base/webfonts_npr.js");
+    break;
+  default:
+    require("./lib/webfonts");
+    break;
+}
+
+// Global vars
+var pymChild = null;
+
+var renderBars = require("./renderBars");
+
+// Initialize the graphic.
+var onWindowLoaded = function() {
+  render(window.DATA);
+
+  window.addEventListener("resize", () => render(window.DATA));
+
+  pym.then(child => {
+    pymChild = child;
+    child.sendHeight();
+  });
+};
+
+// Render the graphic(s)
+var render = function(data) {
+  // Render the chart!
+  var container = "#column-chart";
+  var element = document.querySelector(container);
+  var width = element.offsetWidth;
+  renderBars({
+    container,
+    width,
+    data,
+    labelColumn: "label",
+    valueColumn: "amt"
+  });
+
+  // Update iframe
+  if (pymChild) {
+    pymChild.sendHeight();
+  }
+};
+
+//Initially load the graphic
+window.onload = onWindowLoaded;
