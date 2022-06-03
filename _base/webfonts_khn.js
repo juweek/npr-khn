3a8f02d95eed2bad85549eb33e296ea7b0999b3c
alykat <alykat@morethanthis.net>
2022-06-03 09:23:17 -0400

    prototype switching between NPR and KHN styles

---
+++ _base/webfonts_khn.js
@@ -0,0 +1,15 @@
+var url = "https://apps.npr.org/dailygraphics/graphics/fonts/js/lib/webfont.js";
+var script = document.createElement("script");
+script.src = url;
+document.head.appendChild(script);
+script.onload = function() {
+  WebFont.load({
+      google: {
+          families: [
+              'PT Serif:400,700',
+              'Source Sans Pro:400,700'
+          ]
+      },
+      timeout: 10000
+  });
+};
