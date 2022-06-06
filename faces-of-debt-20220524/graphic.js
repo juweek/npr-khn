133a261bc0f4710d0a24ffb52b316f49cdd0000a
juweek <juweek.adolphe@gmail.com>
2022-06-06 06:54:09 -0400

    loading from spreadsheet

--- faces-of-debt-20220524/graphic.js
+++ faces-of-debt-20220524/graphic.js
@@ -6,6 +6,39 @@ var { isMobile } = require("./lib/breakpoints");
 pym.then(child => {
     child.sendHeight();
 
+    console.log(data)
+
+    let wrapper = document.getElementById('profileCardsWrapper');
+
+    for (const item in data) {
+        let item_data = data[item]
+        let name = item_data.name
+   
+        let newDiv = document.createElement("div");
+        newDiv.className = "profileContainer";
+        newDiv.id = "profileContainer" + item_data.key;
+        newDiv.setAttribute("data-name", name);   
+        newDiv.style.backgroundImage = 'url(./assets/' + name.toLowerCase().replace(/\s/g, '') + ".jpg)";
+        newDiv.style.backgroundSize = "cover";
+        newDiv.style.backgroundPosition = "center";
+        newDiv.style.backgroundRepeat = "no-repeat";
+        console.log('./assets/' + name.toLowerCase().replace(/\s/g, '') + ".jpg")
+
+      
+        let newDivText = document.createElement("h2");
+        newDivText.className = "profileQuote";
+        newDivText.innerHTML = '"' + item_data.quote + '"';
+
+        let newDivLabel = document.createElement("span");
+        newDivLabel.className = "profileAmt";
+        newDivLabel.innerHTML = '$' + item_data.debt;
+
+        newDiv.appendChild(newDivText);
+        newDiv.appendChild(newDivLabel);
+        wrapper.appendChild(newDiv);
+      }
+      
+
     // child.onMessage("on-screen", function(bucket) {
     //     ANALYTICS.trackEvent("on-screen", bucket);
     // });
